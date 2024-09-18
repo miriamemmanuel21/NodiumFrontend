a20f6650c6a4ea7730b307ce7fc69cd490806149
Biokes <106444765+Biokes@users.noreply.github.com>
2024-09-18 15:56:08 +0100

    update chart dependency

--- src/provider_components/forbidden_pages.jsx
+++ src/provider_components/forbidden_pages.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import {useNavigate } from 'react-router-dom';
 import styles from '../index.module.css';
 
-function Invalids() {
+export default function Invalids() {
     const navigate = useNavigate();
     return (
         <div className={`bg-black justify-center items-center flex ${styles.invalid} pt-[]lg:pt-[35vw]`}>
